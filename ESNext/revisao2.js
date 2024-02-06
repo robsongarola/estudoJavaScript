aeac6d25297b983c06960276d74e9e2ae2c29856
robsongarola <149022651+robsongarola@users.noreply.github.com>
2024-02-06 20:11:27 -0300

    Create revisao2.js

---
+++ ESNext/revisao2.js
@@ -0,0 +1,33 @@
+// FUNCTION ARROW
+
+const soma = (a,b)=> a+b;
+
+console.log(soma(5,6)) // A FUNCTION ARROW É SEMPRE ANÔNIMA
+
+// A FUNÇÃO ARROW POSSUI O 'THIS' LÉXICO (SEMPRE VAI APONTAR A ONDE FOI CRIADA)
+
+const lexico = ()=> console.log(this === exports)
+console.log(lexico())
+const lexixo2 = lexico.bind({}) // FORÇANDO A MUDANÇA DO THIS (NÃO VAI FUNCIONAR)
+console.log(lexixo2()) // CONTINUA APONTADO PARA EXPORTS
+
+// PARAMETROS DEFAULT
+
+function log(texto='Node'){
+
+    console.log(texto)
+}
+
+log() // NÃO PASSANDO PARAMETRO A FUNÇÃO USARÁ O DEFAULT
+log('Sou mais Forte') // USARÁ O PARAMETRO PASSADO
+
+// OPERADOR REST (AGRUPAR), NESSE CONTEXTO
+
+function total(...numeros){ // O OPERADOR REST CRIA UM ARRAY DE ELEMENTOS
+    let total = 0;
+    numeros.forEach( n => total+=n) // NESSE CASO PERCORREMOS E SOMAMOS
+
+    return total
+}
+console.log(total(2,3,4,5))
+

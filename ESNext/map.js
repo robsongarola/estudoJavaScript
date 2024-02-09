663c0188fa8c9d212b46d50f9837ee433235b08d
robsongarola <149022651+robsongarola@users.noreply.github.com>
2024-02-08 21:04:02 -0300

    Create map.js

---
+++ ESNext/map.js
@@ -0,0 +1,20 @@
+// A ESTRUTURA MAP É BEM PARECIDA COM O OBJETO. SENDO A PRINCIPAL DIFERENÇA QUE A CHAVE PODE SER : FUNÇÃO
+// OBJETO, STRING E BOLLEAN. E A FORMA DE ACESSÁ-LOS TAMBÉM É DIFERENTE
+
+const diverso = new Map() ; // ISTANCIANDO A ESTRUTURA
+
+diverso.set('123',function(){})
+diverso.set('456',{bola:true})
+console.log(diverso.get('456').bola)
+
+//PASSANDO ELEMENTOS VARIADOS
+
+const chavesVariadas= new Map([
+    [function(){},'funcao'],
+    [{},'Objeto'],
+    [123,'numero']
+])
+
+chavesVariadas.forEach((vl,ch)=>{
+    console.log(ch,vl)
+})

e1e59cc8b2fee399b0d70e7703e45f18378c9ea6
robsongarola <149022651+robsongarola@users.noreply.github.com>
2024-02-19 22:08:19 -0300

    Update promise_4.js

--- ESNext/promise_4.js
+++ ESNext/promise_4.js
@@ -1,6 +1,29 @@
 // COMO CHAMAR VÁRIAS PROMISES E RETONAR QUANDO TODAS ESTIVEREM CUMPRIDA
 
-function gerarNumeros(min,max,tempo){
+function gerarNumerosEntre(min,max,tempo){
+         if (min>max) [min,max]=[max,min]
+    return new Promise((resolve,reject)=>{
 
-    
+        setTimeout(()=>{
+            let aleatorio = parseInt(Math.random()*(max-min+1)+min)
+            resolve(aleatorio) 
+
+        },tempo)
+
+    })
 }
+// VAMOS INVOCAR A FUNÇÃO , PASSANDO DADOS DIVERSOS
+
+function gerarNumeros(){
+
+    return Promise.all([
+        gerarNumerosEntre(1,60,4000),
+        gerarNumerosEntre(1,60,1000),
+        gerarNumerosEntre(1,60,500),
+        gerarNumerosEntre(1,60,3000),
+        gerarNumerosEntre(1,60,100),
+        gerarNumerosEntre(1,60,1500)
+    ])
+}
+
+gerarNumeros().then( numeros => console.log(numeros))

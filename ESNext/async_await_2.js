e17a543fd9ab696d57786c197cbf1b907e557a51
robsongarola <149022651+robsongarola@users.noreply.github.com>
2024-02-22 21:59:38 -0300

    Create async_await_2.js

---
+++ ESNext/async_await_2.js
@@ -0,0 +1,45 @@
+function gerarNumerosEntre(min,max,numerosProibidos){
+    if (min>max) [min,max]=[max,min]
+return new Promise((resolve,reject)=>{
+
+   setTimeout(()=>{
+       let aleatorio = parseInt(Math.random()*(max-min+1)+min)
+       
+       if(numerosProibidos.includes(aleatorio)){ /* SE ACEITAR (POR QUE HÁ OS NÚMEROS ANTERIORMENTES)*/
+
+         reject('Números Proibidos')
+       }else {
+
+        resolve(aleatorio) 
+       }
+
+   },100)
+
+})
+}
+
+// USANDO O TRATAMENTO DE ERRO COM ASYNC/AWAIT
+
+async function gerarMegaSena(qtdeNumeros){
+   
+    try{
+
+
+        const numeros =[];
+    
+        for( let _ of Array(qtdeNumeros).fill()){ // O MÉTODO .FILL() IRÁ GERAR , UMA QUANTIDADE ALEATÓRIO DE NÚMEROS
+    
+            numeros.push( await gerarNumerosEntre(1,60,numeros)) // O AWAIT TEM QUE ESTÁ LIGADO A FUNC QUE RETORNA PROMISE
+    
+        }
+        return numeros
+
+    } catch(e){
+
+        throw "Que chato !!"
+    }
+}
+
+gerarMegaSena(15)
+        .then(console.log)
+        .catch(console.log)
